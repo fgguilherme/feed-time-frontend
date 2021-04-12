8f66828e594ca0554ad8697cb9e31be378f0de94
fgguilherme <fg.guilherme@gmail.com>
2021-04-12 14:35:41 -0400

    add content to page header, Freshworks on left side and language selector on right side

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  username = 'pipoca!';
+  notifications: any;
+
+  constructor(
+    private router: Router,
+    private translateService: TranslateService,
+    public dialog: MatDialog,
+  ) {
+    this.translateService.setDefaultLang('en');
+  }
+
+  ngOnInit(): void {}
+
+  getLanguage(): string {
+    return this.translateService.getDefaultLang().trim();
+  }
+
+  setLanguage(data: string): void {
+
+  }
+}
